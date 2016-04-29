ed89ff8dfb13663a5835479b7e8dfd67af950822
victor <victor6_1@mail.ru>
2016-04-29 18:22:19 +0300

    Task 10

---
+++ TestMaven/src/test/VariablesServletNew.java
@@ -0,0 +1,23 @@
+
+    package test;
+
+    import javax.servlet.ServletException;
+    import javax.servlet.annotation.WebServlet;
+    import javax.servlet.http.HttpServlet;
+    import javax.servlet.http.HttpServletRequest;
+    import javax.servlet.http.HttpServletResponse;
+    import java.io.IOException;
+
+
+    @WebServlet(value = "/myvar")
+    public class VariablesServletNew extends HttpServlet {
+
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            final String[] vars = {"JAVA_HOME","M2_HOME","CATALINA_HOME","USERNAME","Path"};
+            for (String var : vars) {
+                resp.getOutputStream().println(String.format("%s=%s", var, System.getenv(var)));
+            }
+        }
+    }
+
